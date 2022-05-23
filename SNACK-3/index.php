c9a3b2436bef2e9ccdd1d5643fed354ab943855b
Nicolas De Palma <wellcomenicolas@gmail.com>
2022-05-23 11:58:41 +0200

    SNACK 3 -COMPLETE

---
+++ SNACK-3/index.php
@@ -0,0 +1,75 @@
+<!-- ## Snack 3
+
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+ -->
+
+<?php
+$data = [
+
+    '10-01-2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10-02-2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15-05-2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>data</title>
+</head>
+
+<body>
+
+    <?php foreach ($data as $date => $posts) : ?>
+        <h1><?= $date; ?></h1>
+        <?php foreach ($posts as $post) : ?>
+            <ul>
+                <li><?= $key; ?> : <?= $post['title']; ?></li>
+                <li><?= $key; ?> : <?= $post['author']; ?></li>
+                <li><?= $key; ?> : <?= $post['text']; ?></li>
+            </ul>
+        <?php endforeach; ?>
+    <?php endforeach; ?>
+
+</body>
+
+</html>
